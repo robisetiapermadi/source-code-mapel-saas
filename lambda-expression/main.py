c77505e0e9ec4b1c3589b7e22533b3830bfe9b19
Robi Setia Permadi <setiapermadir@gmail.com>
2021-04-27 19:18:03 +0700

    Tambah source code Contoh lambda expression

---
+++ lambda-expression/main.py
@@ -0,0 +1,22 @@
+# Implementasi fungsi map() untuk menguppercase string pada list
+
+words = ['hati','yang','luka','tenda','biru']
+
+wordsUppercase = list(map(lambda x: x.upper() , words))
+
+print(wordsUppercase)
+
+# implementasi fungsi filter() untuk mengfilter hanya bilangan ganjil 
+
+bilangan = [0, 1, 2, 3, 5, 8, 13]
+  
+hasil = filter(lambda x: x % 2 != 0, bilangan)
+print(list(hasil))
+
+
+# impolementasi fungsi reduce() untuk menjumlahkan semua elemen pada list
+from functools import reduce
+
+numbers = [0,1,2,3,4]
+
+print(reduce(lambda x,y: x+y, numbers))
